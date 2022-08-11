409dd597a3d799d937a8439fffbdfb4aefcab81c
Diego-Aleixo-EM <diego.aleixo@escolarmanager.com.br>
2022-08-11 14:41:29 -0300

    Cria classe para transferência de informações

---
+++ src/CORE/DTO/DTODadosNotaFiscal.cs
@@ -0,0 +1,11 @@
+﻿namespace CORE.DTO;
+
+public class DTODadosNotaFiscal
+{
+	public string Usuario { get; set; } = string.Empty;
+	public string Senha { get; set; } = string.Empty;
+	public string XmlEnvio { get; set; } = string.Empty;
+	public string XmlResposta { get; set; } = string.Empty;
+	public bool EhEnvioProducao { get; set; }
+	public List<string> Inconsistencias { get; } = new();
+}
